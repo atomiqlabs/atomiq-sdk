dad2edadb690e5590cdc9bd6531da8c9ece16808
adambor <adamborteam@gmail.com>
2026-03-18 11:07:50 +0100

    Initial draft

--- src/types/SwapExecutionAction.ts
+++ src/types/SwapExecutionAction.ts
@@ -1,6 +1,72 @@
 import {ChainType} from "@atomiqlabs/base";
 import {Transaction} from "@scure/btc-signer";
 
+
+
+export type SwapExecutionActionSendToAddress = {
+    type: "SendToAddress",
+    name: string,
+    description: string,
+    chain: "LIGHTNING" | "BITCOIN",
+    txs: {
+        type: "BOLT11_PAYMENT_REQUEST" | "ADDRESS",
+        address: string,
+        hyperlink: string,
+        amount: number
+    }[]
+}
+
+/**
+ * Swap execution action on on-chain Bitcoin, has following types:
+ * - `"ADDRESS"` - Destination bitcoin address and BTC amount to be sent
+ * - `"FUNDED_PSBT"` - A ready to sign PSBT with the inputs populated from the provided bitcoin wallet address
+ * - `"RAW_PSBT"` - Raw PSBT without the inputs, the implementor needs to add the input UTXOs before signing
+ *  the transaction
+ *
+ * @category Swap Actions
+ */
+export type SwapExecutionActionSignPSBT<
+  T extends "FUNDED_PSBT" | "RAW_PSBT" = "FUNDED_PSBT" | "RAW_PSBT"
+> = {
+    type: "SignPSBT",
+    name: string,
+    description: string,
+    chain: "BITCOIN",
+    txs: (T extends "FUNDED_PSBT" ? {
+        type: "FUNDED_PSBT",
+        psbt: Transaction,
+        psbtHex: string,
+        psbtBase64: string,
+        signInputs: number[]
+    } : {
+        type: "RAW_PSBT",
+        psbt: Transaction,
+        psbtHex: string,
+        psbtBase64: string,
+        in1sequence: number
+    })[]
+}
+
+export type SwapExecutionActionSignSmartChainTx<T extends ChainType> = {
+    type: "SignSCTX",
+    name: string,
+    description: string,
+    chain: T["ChainId"],
+    txs: T["TX"][],
+    sendTxs: () => Promise<string[]>,
+    waitTillTxsConfirmed: () => Promise<void>
+}
+
+
+export type SwapExecutionActionWait<T extends ChainType> = {
+    type: "Wait",
+    name: string,
+    description: string,
+    wait: () => Promise<void>
+    expectedTime: number,
+    pollTime: number
+}
+
 /**
  * Swap execution action requiring a payment of the provided bolt11 lightning network invoice
  *
@@ -15,41 +81,6 @@ export type SwapExecutionActionLightning = {
         address: string,
         hyperlink: string
     }[]
-}
-
-/**
- * Swap execution action on on-chain Bitcoin, has following types:
- * - `"ADDRESS"` - Destination bitcoin address and BTC amount to be sent
- * - `"FUNDED_PSBT"` - A ready to sign PSBT with the inputs populated from the provided bitcoin wallet address
- * - `"RAW_PSBT"` - Raw PSBT without the inputs, the implementor needs to add the input UTXOs before signing
- *  the transaction
- *
- * @category Swap Actions
- */
-export type SwapExecutionActionBitcoin<
-    T extends "ADDRESS" | "FUNDED_PSBT" | "RAW_PSBT" = "ADDRESS" | "FUNDED_PSBT" | "RAW_PSBT"
-> = {
-    name: "Payment",
-    description: string,
-    chain: "BITCOIN",
-    txs: (T extends "ADDRESS" ? {
-        type: "ADDRESS",
-        amount: number,
-        address: string,
-        hyperlink: string
-    } : T extends "FUNDED_PSBT" ? {
-        type: "FUNDED_PSBT",
-        psbt: Transaction,
-        psbtHex: string,
-        psbtBase64: string,
-        signInputs: number[]
-    } : {
-        type: "RAW_PSBT",
-        psbt: Transaction,
-        psbtHex: string,
-        psbtBase64: string,
-        in1sequence: number
-    })[]
 }
 
 /**

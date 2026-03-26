ff3a6d68ed37907c4434366b3d34b8f8f1c95658
adambor <adamborteam@gmail.com>
2026-03-27 00:43:29 +0100

    Add parseAddress and getSpendableBalance API endpoints

--- src/api/ApiEndpoints.ts
+++ src/api/ApiEndpoints.ts
@@ -1,7 +1,7 @@
 import {SwapExecutionStep} from "../types/SwapExecutionStep";
 import {SerializedAction} from "./SerializedAction";
 import {SwapExecutionAction} from "../types/SwapExecutionAction";
-import {ApiAmount, ApiToken} from "./ApiTypes";
+import {ApiAmount, ApiLNURL, ApiToken} from "./ApiTypes";
 
 export type SwapOutputBase = {
     swapId: string;
@@ -184,6 +184,54 @@ export type GetSwapLimitsOutput = {
         min: ApiAmount;
         max?: ApiAmount;
     };
+}
+
+/**
+ * Input for parsing an address-like string supported by the SDK
+ *
+ * @category API
+ */
+export type ParseAddressInput = {
+    address: string;
+}
+
+/**
+ * Output from address parser endpoint
+ *
+ * @category API
+ */
+export type ParseAddressOutput = {
+    address: string;
+    type: string;
+    lnurl?: ApiLNURL;
+    min?: ApiAmount;
+    max?: ApiAmount;
+    amount?: ApiAmount;
+};
+
+/**
+ * Input for querying spendable wallet balance
+ *
+ * @category API
+ */
+export type GetSpendableBalanceInput = {
+    wallet: string;
+    token: string;
+    targetChain?: string;
+    gasDrop?: boolean;
+    feeRate?: number;
+    minFeeRate?: number;
+    feeMultiplier?: number;
+}
+
+/**
+ * Output from spendable balance endpoint
+ *
+ * @category API
+ */
+export type GetSpendableBalanceOutput = {
+    balance: ApiAmount;
+    feeRate?: number;
 }
 
 /**

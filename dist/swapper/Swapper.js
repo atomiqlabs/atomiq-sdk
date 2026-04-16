62adc04f9ce52a56422b7c3661668572475741a2
adambor <adamborteam@gmail.com>
2026-04-17 00:41:05 +0200

    Add isInitialized() function to swapper

--- dist/swapper/Swapper.js
+++ dist/swapper/Swapper.js
@@ -203,7 +203,7 @@ class Swapper extends events_1.EventEmitter {
         });
     }
     async _init() {
-        this.logger.debug("init(): Initializing swapper...");
+        this.logger.debug("init(): Initializing swapper");
         const abortController = new AbortController();
         const promises = [];
         let automaticClockDriftCorrectionPromise = undefined;
@@ -305,6 +305,12 @@ class Swapper extends events_1.EventEmitter {
             delete this.initPromise;
             throw e;
         }
+    }
+    /**
+     * Whether the SDK is initialized (after {@link init} is called)
+     */
+    isInitialized() {
+        return this.initialized;
     }
     /**
      * Stops listening for onchain events and closes this Swapper instance
